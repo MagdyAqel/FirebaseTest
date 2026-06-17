8e8648d6795954166f85b4560a68bcbad3a6dc4f
MagdyAqel <111590180+MagdyAqel@users.noreply.github.com>
2026-06-17 14:25:40 +0300

    Add Firebase config placeholder

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "ضع-apiKey-هنا",
+  authDomain: "ضع-authDomain-هنا",
+  projectId: "ضع-projectId-هنا",
+  storageBucket: "ضع-storageBucket-هنا",
+  messagingSenderId: "ضع-messagingSenderId-هنا",
+  appId: "ضع-appId-هنا"
+};
