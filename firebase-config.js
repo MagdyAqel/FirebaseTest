b70e3cce6e676d4611c6e12caa6fa356f0a49b8c
MagdyAqel <111590180+MagdyAqel@users.noreply.github.com>
2026-06-25 15:40:46 +0300

    Configure Realtime Database

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCIOECUiEvL9HDqjW0X7U3ekW5qeGxIEUk",
   authDomain: "fir-test-e7a14.firebaseapp.com",
+  databaseURL: "https://fir-test-e7a14-default-rtdb.firebaseio.com",
   projectId: "fir-test-e7a14",
   storageBucket: "fir-test-e7a14.firebasestorage.app",
   messagingSenderId: "828970446402",
   appId: "1:828970446402:web:68dd31917301193ebc47db"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
